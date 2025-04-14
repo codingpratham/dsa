2e77745a4ef210654515804e4628204785912563
codingpratham <coding.job17@gmail.com>
2025-04-14 21:14:30 +0530

    c++

---
+++ week-1/hello.cpp
@@ -0,0 +1,21 @@
+#include  <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    vector<int> arr;
+
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    arr.push_back(40);
+
+    arr.back
+
+    for(int i=0 ; i < arr.size() ; i++ ){
+        cout << arr[i] << endl ;
+    }
+
+    return 0;
+}
